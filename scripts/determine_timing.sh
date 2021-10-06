48286ed3e4b146a3aab926d741687f6912fb0d78
Wouter Deconinck <wouter.deconinck@umanitoba.ca>
2021-10-06 17:27:50 +0000

    Add support for single event timings

--- scripts/determine_timing.sh
+++ scripts/determine_timing.sh
@@ -5,13 +5,25 @@ while IFS="," read file nevents ; do
   cifile=${file/EVGEN/EVGEN\/CI}
   cidir=$(dirname ${cifile})
   mkdir -p ${cidir}
-  mc head -n $nlines S3/eictest/ATHENA/$file > ${cifile}
-  n=$(grep ^E ${cifile} | wc -l)
-  test $n -gt 0 || exit -1
+  type=${file##*.}
+  if [ "${type}" == "hepmc" ] ; then
+    mc head -n $nlines S3/eictest/ATHENA/$file > ${cifile}
+    n=$(grep ^E ${cifile} | wc -l)
+    n=$((n-1)) # last event is corrupted
+    test $n -gt 0 || exit -1
+    type="hepmc3"
+  elif [ "${type}" == "steer" ] ; then
+    mc cp S3/eictest/ATHENA/$file > ${cifile}
+    n=$((nlines/10)) # 10 lines per event equivalent
+  else
+    echo "Error: extension not recognized"
+    exit -1
+  fi
+  logfile=results/logs/${cifile}.out
+  mkdir -p $(dirname ${logfile})
   t1=$(date +%s.%N)
-  mkdir -p results/logs/
-  /opt/campaigns/hepmc3/scripts/run.sh ${cifile} $((nevents-1)) 2>&1 > results/logs/${cifile}.out
+  /opt/campaigns/${type}/scripts/run.sh ${cifile} ${n} 2>&1 > ${logfile}
   t2=$(date +%s.%N)
-  dt=$(echo "scale=5; 1000.0*($t2-$t1)/($n)" | bc -l)
+  dt=$(echo "scale=5; ($t2-$t1)/($n)" | bc -l)
   echo "$file,$nevents,$dt"
 done
