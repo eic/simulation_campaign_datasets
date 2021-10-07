3b75d3ac48b25e6d12c6c84d061a0efb4c895743
Wouter Deconinck <wdconinc@gmail.com>
2021-10-07 15:22:59 -0500

    Correction for initialization time

--- scripts/determine_timing.sh
+++ scripts/determine_timing.sh
@@ -29,5 +29,12 @@ mkdir -p $(dirname ${logfile})
 t1=$(date +%s.%N)
 /opt/campaigns/${type}/scripts/run.sh ${cifile} ${n} 2>&1 > ${logfile}
 t2=$(date +%s.%N)
-dt=$(echo "scale=5; ($t2-$t1)/($n)" | bc -l)
-echo "$file,$nevents,$dt"
+dt0n=$(echo "scale=5; ($t2-$t1)" | bc -l)
+# initialization correction
+t1=$(date +%s.%N)
+/opt/campaigns/${type}/scripts/run.sh ${cifile} 1 2>&1 > /dev/null
+t2=$(date +%s.%N)
+dt01=$(echo "scale=5; ($t2-$t1)" | bc -l)
+dt1=$(echo "scale=5; ($dt0n-$dt01)/($n-1)" | bc -l)
+dt0=$(echo "scale=5; ($dt01-$dt1)" | bc -l)
+echo "$file,$nevents,$dt0,$dt1"
