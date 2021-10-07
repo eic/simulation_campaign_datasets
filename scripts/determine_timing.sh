ced0230e6a408e6912c7d87ff5c5b7876f04169b
Wouter Deconinck <wouter.deconinck@umanitoba.ca>
2021-10-07 16:25:07 +0000

    hide mc output and test if file create

--- scripts/determine_timing.sh
+++ scripts/determine_timing.sh
@@ -13,7 +13,8 @@ while IFS="," read file nevents ; do
     test $n -gt 0 || exit -1
     type="hepmc3"
   elif [ "${type}" == "steer" ] ; then
-    mc cp S3/eictest/ATHENA/${file} ${cifile}
+    mc cp -q S3/eictest/ATHENA/${file} ${cifile} > /dev/null
+    test -f ${cifile}
     n=$((nlines/10)) # 10 lines per event equivalent
     type="single"
   else
