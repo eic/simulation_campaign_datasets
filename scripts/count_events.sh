cebe49170e31918dd59a6aae003c54a5b8ffa0f0
Wouter Deconinck <wouter.deconinck@umanitoba.ca>
2021-10-20 20:56:07 +0000

    Output n_lines_per_event in count_events.sh

--- scripts/count_events.sh
+++ scripts/count_events.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 file=${1?Specify filename}
-n_events=${2:-} # allow empty
+nevents=${2:-} # allow empty
+n_lines_per_event=${3:-} # allow empty
 
 # if nevents not known
-if [ -z "$n_events" ] ; then
+if [ -z "$nevents" -o -z "${n_lines_per_event}" ] ; then
   # count events
-  n_events=$(mc cat S3/eictest/ATHENA/$file | grep ^E | wc -l)
+  nevents=$(mc cat S3/eictest/ATHENA/$file | grep ^E | wc -l)
+  # count events in 10k lines
   n_events_10k=$(mc head -n 10000 S3/eictest/ATHENA/$file | grep ^E | wc -l)
   n_lines_per_event=$((10000/n_events_10k))
 fi
 
 # output
-echo "$file,$n_events,$n_lines_per_event"
+echo "$file,$nevents,$n_lines_per_event"
