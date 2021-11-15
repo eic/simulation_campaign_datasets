3be5319db6fca23d4b58ccf5955f4bcf95cc9c4e
Wouter Deconinck <wdconinc@gmail.com>
2021-11-15 04:23:54 +0000

    gzip everything; 1 parallel job

--- scripts/gzip_hepmc.sh
+++ scripts/gzip_hepmc.sh
@@ -5,8 +5,10 @@ nevents=${2:-} # allow empty
 n_lines_per_event=${3:-} # allow empty
 
 if [ -z "$(mc ls S3rw/eictest/ATHENA/${file}.gz)" ] ; then
-  echo "gzip $file"
+  echo "gzip ${file}"
   mc ls S3rw/eictest/ATHENA/${file}
   mc cat S3rw/eictest/ATHENA/${file} | gzip -c | mc pipe S3rw/eictest/ATHENA/${file}.gz
   mc ls S3rw/eictest/ATHENA/${file}.gz
+else
+  echo "${file}.gz already exists"
 fi
