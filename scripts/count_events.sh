d80ee282b3f7332721e33f3c0dcf18887de9c903
Wouter Deconinck <wdconinc@gmail.com>
2021-11-17 15:29:05 -0600

    Support for counting on gzipped files

--- scripts/count_events.sh
+++ scripts/count_events.sh
@@ -4,14 +4,24 @@ file=${1?Specify filename}
 nevents=${2:-} # allow empty
 n_lines_per_event=${3:-} # allow empty
 
+if [ -z "$(mc ls S3rw/eictest/ATHENA/${file}.gz)" ] ; then
+  # regular
+  GUNZIP=(cat)
+else
+  # gzipped
+  GUNZIP=(gunzip -c)
+fi
+
 # if nevents not known
 if [ -z "${nevents}" ] ; then
   # count events
-  nevents=$(mc cat S3/eictest/ATHENA/${file} | grep ^E | wc -l)
+  nevents=$(mc cat S3/eictest/ATHENA/${file} | ${GUNZIP[@]} | grep ^E | wc -l)
 fi
+
+# if n_lines_per_event not known
 if [ -z "${n_lines_per_event}" ] ; then
   # count events in 10k lines
-  n_events_10k=$(mc head -n 10000 S3/eictest/ATHENA/${file} | grep ^E | wc -l)
+  n_events_10k=$(mc cat S3/eictest/ATHENA/${file} | ${GUNZIP[@]} | head -n 10000 | grep ^E | wc -l)
   n_lines_per_event=$((10000/n_events_10k))
 fi
 
