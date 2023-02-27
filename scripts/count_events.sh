1d468f9c6b3a43c7d1312c65a2eb806749debd58
Wouter Deconinck <wdconinc@gmail.com>
2023-02-27 16:44:27 +0000

    count_events.sh: unparse S3 urls

--- scripts/count_events.sh
+++ scripts/count_events.sh
@@ -9,6 +9,10 @@ ext=${3?Specify extension}
 nevents=${4:-} # allow empty
 n_lines_per_event=${5:-} # allow empty
 
+s3prefix="S3/eictest/EPIC/EVGEN"
+s3https="s3https://eics3.sdcc.bnl.gov:9000/eictest/EPIC/EVGEN"
+xrootd="root://dtn-eic.jlab.org//work/eic2/EPIC/EVGEN"
+
 if [[ "${ext}" =~ ^hepmc\.gz$ ]] ; then
   GUNZIP=(gunzip -c)
 else
@@ -17,20 +21,23 @@ fi
 
 # loop over glob expression
 dir=$(dirname ${file})
-for s3file in $(mc find S3/eictest/EPIC/EVGEN/${dir} -name ${file}.${ext}) ; do
+for s3file in $(mc find ${s3prefix}/${dir} -name ${file}.${ext}) ; do
+
+  # reconstruct file
+  file=${s3file/${s3prefix}/}
 
   # if nevents not known
   if [[ "${nevents}" == 0 ]] ; then
     # count events
-    nevents=$(mc cat ${s3file} | ${GUNZIP[@]} | grep ^E | wc -l)
+    nevents=$(mc cat ${s3prefix}/${s3file} | ${GUNZIP[@]} | grep ^E | wc -l)
   fi
 
   # if hepmc3.tree.root file
   if [[ "${ext}" =~ ^hepmc3\.tree.root$ ]] ; then
     # get entries from xrootd
-    nevents1=$(root -l -b -q root://dtn-eic.jlab.org//work/eic2/EPIC/EVGEN/${file}.${ext} -e 'cout << hepmc3_tree->GetEntries() << endl;' | tail -n1)
+    nevents1=$(root -l -b -q ${xrootd}/${file}.${ext} -e 'cout << hepmc3_tree->GetEntries() << endl;' | tail -n1)
     # get entries from S3
-    nevents2=$(root -l -b -q s3https://eics3.sdcc.bnl.gov:9000/eictest/EPIC/EVGEN/${file}.${ext} -e 'cout << hepmc3_tree->GetEntries() << endl;' | tail -n1)
+    nevents2=$(root -l -b -q ${s3https}/${file}.${ext} -e 'cout << hepmc3_tree->GetEntries() << endl;' | tail -n1)
     # compare
     test ${nevents1} -eq ${nevents2}
     nevents=${nevents1}
@@ -40,7 +47,7 @@ for s3file in $(mc find S3/eictest/EPIC/EVGEN/${dir} -name ${file}.${ext}) ; do
   # if n_lines_per_event not known
   if [ -z "${n_lines_per_event}" ] ; then
     # count events in 10k lines
-    n_events_10k=$(mc cat ${s3file} | ${GUNZIP[@]} | head -n 10000 | grep ^E | wc -l)
+    n_events_10k=$(mc cat ${s3prefix}/${s3file} | ${GUNZIP[@]} | head -n 10000 | grep ^E | wc -l)
     n_lines_per_event=$((10000/n_events_10k))
   if
 
