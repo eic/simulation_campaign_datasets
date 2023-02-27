76295ab7c3b0102fa03512e87b6884e1714779cc
Wouter Deconinck <wdconinc@gmail.com>
2023-02-27 18:59:33 +0000

    feat: glob in separate script

--- scripts/count_events.sh
+++ scripts/count_events.sh
@@ -19,39 +19,30 @@ else
   GUNZIP=(cat)
 fi
 
-# loop over glob expression
-dir=$(dirname ${file})
-for s3file in $(mc find ${s3prefix}/${dir} -name ${file}.${ext}) ; do
+# if nevents not known
+if [[ "${nevents}" == 0 ]] ; then
+  # count events
+  nevents=$(mc cat ${s3prefix}/${s3file} | ${GUNZIP[@]} | grep ^E | wc -l)
+fi
 
-  # reconstruct file
-  file=${s3file/${s3prefix}/}
+# if hepmc3.tree.root file
+if [[ "${ext}" =~ ^hepmc3\.tree.root$ ]] ; then
+  # get entries from xrootd
+  nevents1=$(root -l -b -q ${xrootd}/${file}.${ext} -e 'cout << hepmc3_tree->GetEntries() << endl;' | tail -n1)
+  # get entries from S3
+  nevents2=$(root -l -b -q ${s3https}/${file}.${ext} -e 'cout << hepmc3_tree->GetEntries() << endl;' | tail -n1)
+  # compare
+  test ${nevents1} -eq ${nevents2}
+  nevents=${nevents1}
+  n_lines_per_event=0
+fi
 
-  # if nevents not known
-  if [[ "${nevents}" == 0 ]] ; then
-    # count events
-    nevents=$(mc cat ${s3prefix}/${s3file} | ${GUNZIP[@]} | grep ^E | wc -l)
-  fi
+# if n_lines_per_event not known
+if [ -z "${n_lines_per_event}" ] ; then
+  # count events in 10k lines
+  n_events_10k=$(mc cat ${s3prefix}/${s3file} | ${GUNZIP[@]} | head -n 10000 | grep ^E | wc -l)
+  n_lines_per_event=$((10000/n_events_10k))
+if
 
-  # if hepmc3.tree.root file
-  if [[ "${ext}" =~ ^hepmc3\.tree.root$ ]] ; then
-    # get entries from xrootd
-    nevents1=$(root -l -b -q ${xrootd}/${file}.${ext} -e 'cout << hepmc3_tree->GetEntries() << endl;' | tail -n1)
-    # get entries from S3
-    nevents2=$(root -l -b -q ${s3https}/${file}.${ext} -e 'cout << hepmc3_tree->GetEntries() << endl;' | tail -n1)
-    # compare
-    test ${nevents1} -eq ${nevents2}
-    nevents=${nevents1}
-    n_lines_per_event=0
-  fi
-
-  # if n_lines_per_event not known
-  if [ -z "${n_lines_per_event}" ] ; then
-    # count events in 10k lines
-    n_events_10k=$(mc cat ${s3prefix}/${s3file} | ${GUNZIP[@]} | head -n 10000 | grep ^E | wc -l)
-    n_lines_per_event=$((10000/n_events_10k))
-  if
-
-  # output
-  echo "$file,$ext,$nevents,$n_lines_per_event" | tee -a "${out}"
-
-done
+# output
+echo "$file,$ext,$nevents,$n_lines_per_event" | tee -a "${out}"
