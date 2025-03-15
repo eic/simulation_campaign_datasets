84b9a3f722d651a50b2f70d55a0b97852c6406de
Sakib Rahman <rahmans@myumanitoba.ca>
2025-03-15 12:49:03 +0000

    Switch to volatile

--- scripts/count_events.sh
+++ scripts/count_events.sh
@@ -9,7 +9,7 @@ ext=${3?Specify extension}
 nevents=${4:-} # allow empty
 n_lines_per_event=${5:-} # allow empty
 
-xrootd="root://dtn-eic.jlab.org//work/eic2/EPIC/EVGEN"
+xrootd="root://dtn-eic.jlab.org//volatile/eic/EPIC/EVGEN"
 
 # if hepmc3.tree.root file
 if [[ "${ext}" =~ ^hepmc3\.tree.root$ ]] ; then
